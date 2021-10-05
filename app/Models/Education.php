cb2bd9a827849f1a080639767f4835d96aeb6263
Willem-Jaap Klepper <wklepper@emerit.nl>
2021-10-05 09:48:52 +0200

    Add Educations controller, model & migration.

---
+++ app/Models/Education.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Education extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'date_start', 'date_end'
+    ];
+}
---
+++ database/migrations/2021_10_05_073043_create_educations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('educations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 16);
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('educations');
+    }
+}
