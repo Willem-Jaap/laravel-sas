0e4230f8890199222f44f3414b134c314b4b4372
Willem-Jaap Klepper <wklepper@emerit.nl>
2021-11-02 12:13:13 +0100

    Add edit template for results, add functionality to forms to render value's in form.

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,17 +1,19 @@
-@foreach ($formInputs as $formInput)
-<div>
-    @if (isset($formInput['type']) && isset($formInput['options']) && $formInput['type'] === 'select')
-    <select name="{{ $formInput['name'] }}" id="{{ $formInput['name'] }}" class="appearance-none rounded-none text-sm relative block w-full p-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
-    focus:z-10 sm:text-sm  {{ $loop->first ? 'rounded-t-md' : '' }} {{ $loop->last ? 'rounded-b-md' : '' }}">
-        @foreach (${$formInput['options']} as $option)
-        <option value="{{ $option->{$formInput['key']} }}">{{ $option->{$formInput['value']} }}</option>
-        @endforeach
-        
-    </select>
-    @else
-    <input id="{{ $formInput['name'] }}" type="{{ isset($formInput['type']) ? $formInput['type'] : 'text' }}" placeholder="{{ $formInput['label'] }}"
-        class="appearance-none rounded-none text-sm relative block w-full p-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm {{ $loop->first ? 'rounded-t-md' : '' }} {{ $loop->last ? 'rounded-b-md' : '' }}"
-        name="{{ $formInput['name'] }}" value="" autocomplete="{{ $formInput['name'] }}" autofocus>
+@foreach ($formInputs as $key => $formInput)
+    @if (gettype($key) === 'integer')
+    <div>
+        @if (isset($formInput['type']) && isset($formInput['options']) && $formInput['type'] === 'select')
+        <select name="{{ $formInput['name'] }}" id="{{ $formInput['name'] }}" class="appearance-none rounded-none text-sm relative block w-full p-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+        focus:z-10 sm:text-sm  {{ $loop->first ? 'rounded-t-md' : '' }} {{ $loop->last ? 'rounded-b-md' : '' }}">
+            @foreach (${$formInput['options']} as $option)
+            <option value="{{ $option->{$formInput['key']} }}" {{ $option->{$formInput['key']} == ${$formInputs['model']}->{$formInput['name']} ? 'selected="selected"' : false }}">{{ $option->{$formInput['value']} }}</option>
+            @endforeach
+            
+        </select>
+        @else
+        <input id="{{ $formInput['name'] }}" type="{{ isset($formInput['type']) ? $formInput['type'] : 'text' }}" placeholder="{{ $formInput['label'] }}"
+            class="appearance-none rounded-none text-sm relative block w-full p-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm {{ $loop->first ? 'rounded-t-md' : '' }} {{ $loop->last ? 'rounded-b-md' : '' }}"
+            name="{{ $formInput['name'] }}" value="{{ $formInputs['method'] == 'update' ? ${$formInputs['model']}->{$formInput['name']} : '' }}" autocomplete="{{ $formInput['name'] }}" autofocus>
+        @endif
+    </div>
     @endif
-</div>
 @endforeach
